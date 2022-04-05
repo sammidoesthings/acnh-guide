08c508b09005470fcfc6cfc4259a041a6418537f
Samantha <samanthaltl.sfh@gmail.com>
2022-04-04 17:28:44 -0700

    villager list sorted + images rendering!

--- src/components/Villagers.js
+++ src/components/Villagers.js
@@ -10,8 +10,19 @@ function Villagers() {
     useEffect(() => {
         axios.get(`${baseURL}${villagersURL}${keyConnect}${apiKey}`)
             .then(response => {
+                //filtering between this and response.data consoled
+                    const responseData = response.data;
+                    // const filterForAce = responseData.filter(findAce => findAce.name === "Ace")
+                    // console.log(filterForAce)
+                    //correctly filters for all NH appearances!
+                    const filter = "NH";
+                    const filteredResult = responseData.filter((person) => { 
+                        return (person.appearances.indexOf(filter) >= 0)
+                    })
+
+                // console.log(filteredResult)
                 // console.log(response.data)
-                setVillagerData(response.data)
+                setVillagerData(filteredResult)
                 return villagerData
             })
             .catch(error => {
@@ -19,10 +30,11 @@ function Villagers() {
             })
     },[])
 
+    // console.log(villagerData)
 
-    console.log(villagerData)
-
-
+    // const filtered = villagerData.filter(species => species.species === 'Bird')
+    // setVillagerData(filtered)
+    // console.log(filtered)
 
 
                 //RETURN BELOW HERE
@@ -36,15 +48,15 @@ function Villagers() {
             {villagerData.map(individual => {
                 return(
                     <div individual={individual} key={individual.url}>
+                        <img src={individual.image_url} alt="villager profile" width={50} length={100}/>
                         <p>{individual.name}</p>
                         <p>{individual.species}</p>
                         <p>{individual.personality}</p>
-                        <p>{individual.appearances}</p>
+                        <p>{individual.birthday_month} {individual.birthday_day}</p>
                         <p>---</p>
                     </div>
                 )
             })}
-
 
         </div>
     )
